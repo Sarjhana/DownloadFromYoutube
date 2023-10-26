732a0590c7da469bbd5528e36dcb97c3a58c1fb4
Sarjhana <41203054+Sarjhana@users.noreply.github.com>
2023-10-26 19:49:08 +0100

    modified to download as mp3 or mp4

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -1,14 +1,43 @@
 from pytube import YouTube
+from moviepy.editor import *
+
+def download_as_mp3(audio_stream, save_path):
+    audio_file_path = audio_stream.download(save_path)
+    audio_clip = AudioFileClip(audio_file_path)
+    audio_clip.write_audiofile(audio_file_path.replace(".webm", ".mp3").replace(".mp4", ".mp3"))
+    audio_clip.close()
+    os.remove(audio_file_path)
+
+def download_as_mp4(video_stream, save_path):
+    video_stream.download(save_path)
 
 try:
-    yt = YouTube(input("Enter link :"))
+    yt = YouTube(input("Enter link: "))
 except:
     print("Connection Error")
+    exit()
 
-myFile = yt.streams.first()
- 
-try:  
-    myFile.download('/Users/sarjhana/Documents/')  
-except:  
-    print("Some Error!")  
+format_choice = input("Which format do you want to download? (mp4/mp3): ").strip().lower()
+
+if format_choice == 'mp3':
+    audio_stream = yt.streams.filter(only_audio=True).first()
+    if audio_stream:
+        try:
+            download_as_mp3(audio_stream, '/Users/sarjhana/Documents/')
+        except:
+            print("Some Error!")
+    else:
+        print("Audio stream not found!")
+elif format_choice == 'mp4':
+    video_stream = yt.streams.filter(file_extension='mp4').first()
+    if video_stream:
+        try:
+            download_as_mp4(video_stream, '/Users/sarjhana/Documents/')
+        except:
+            print("Some Error!")
+    else:
+        print("MP4 stream not found!")
+else:
+    print("Invalid choice!")
+
 print('Task Completed!')
